366f24aa25134fe930da0dfd99c930c00818abf0
Abhilash30 <67117287+Abhilash30@users.noreply.github.com>
2020-07-27 18:49:20 +0530

    Ef

--- scene's/StaticBody2D.gd
+++ scene's/StaticBody2D.gd
@@ -4,8 +4,8 @@ extends StaticBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var anim = get_node("AnimationPlayer")
-
+onready var animation_player = $AnimationPlayer
+onready var Black = $CanvasModulate/Black
 # Called when the node enters the scene tree for the first time.
 func on_ready():
 	
@@ -16,7 +16,11 @@ func on_ready():
 
 func _on_Area2D_body_entered(body):
 	if "Player" in body.name:
-		anim.play("FadeOut (copy)")
-		yield(get_tree().create_timer(1), "timeout")
+		
+		yield(get_tree().create_timer(0.1), "timeout")
+		animation_player.play("FadeIn")
+	  
+		yield(animation_player, "animation_finished")
 		get_tree().change_scene("res://scene's/Node.tscn")
+		animation_player.play_backwards("FadeIn")
 	pass # Replace with function body.
