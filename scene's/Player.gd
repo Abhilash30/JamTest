c031e2825b1b29318ff36e62205a1e486ff29a87
Abhilash30 <67117287+Abhilash30@users.noreply.github.com>
2020-07-27 16:51:55 +0530

    Move

--- scene's/Player.gd
+++ scene's/Player.gd
@@ -7,7 +7,7 @@ var UP = Vector2(0, -1)
 
 # Called when the node enters the scene tree for the first time.
 func _physics_process(delta):
-	motion.y += 10
+	motion.y += 9
 	if Input.is_action_pressed("ui_right"):
 		motion.x = Speed
 	elif Input.is_action_pressed("ui_left"):
@@ -18,7 +18,7 @@ func _physics_process(delta):
 	
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
-			motion.y = -260
+			motion.y = -200
 			
 	pass # Replace with function body.
 
