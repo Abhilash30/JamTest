ff4aba0bdfe80766542a123f805741a839dd1a9d
Abhilash30 <67117287+Abhilash30@users.noreply.github.com>
2020-07-27 14:06:48 +0530

    Movement and shadow's

---
+++ scene's/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+
+var motion = Vector2()
+var Speed = 200
+var UP = Vector2(0, -1)
+
+# Called when the node enters the scene tree for the first time.
+func _physics_process(delta):
+	motion.y += 10
+	if Input.is_action_pressed("ui_right"):
+		motion.x = Speed
+	if Input.is_action_pressed("ui_left"):
+		motion.x = -Speed
+	else:
+		motion.x = 0
+	move_and_slide(motion, UP)
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = -200
+			
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
